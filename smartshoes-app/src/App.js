3adc977d08d5358f81192b795df886832a66f926
jumanchuk <juryumanchuk@gmail.com>
2021-10-03 09:16:04 -0300

    Desafío: Crear la App utilizando el CLI

---
+++ smartshoes-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is my React E-comerce Project - SmartShoes.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/jumanchuk/react-portfolio/smartshoes-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github Link
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
